ffcfa4654ca54d83d012eda2a2aa061dcc4b1e42
Angel Serrano <39681418+unmexicano@users.noreply.github.com>
2022-02-16 00:02:45 -0600

    update_cat

--- orders/migrations/0006_userorder.py
+++ orders/migrations/0006_userorder.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('orders', '0005_auto_20191129_1525'),
+        ('orders', '0002_manjus'),
     ]
 
     operations = [

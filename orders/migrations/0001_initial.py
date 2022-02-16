8c2a304dc522d8f8411b47100a2adc898ecd25f0
Angel Serrano <39681418+unmexicano@users.noreply.github.com>
2022-02-16 00:21:43 -0600

    update

--- orders/migrations/0001_initial.py
+++ orders/migrations/0001_initial.py
@@ -12,11 +12,11 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='categorias',
+            name='categoria',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('category_title', models.CharField(max_length=200)),
-                ('category_gif', models.CharField(max_length=200)),
+                ('category_png', models.CharField(max_length=200)),
                 ('category_description', models.TextField()),
             ],
         ),

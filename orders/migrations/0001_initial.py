b6154e60cc37b058fe17b1faa2d8a84262c77771
Angel Serrano <39681418+unmexicano@users.noreply.github.com>
2022-02-15 23:48:52 -0600

    db updates

--- orders/migrations/0001_initial.py
+++ orders/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Category',
+            name='categorias',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('category_title', models.CharField(max_length=200)),
--- orders/migrations/0003_auto_20191129_1427.py
+++ orders/migrations/0003_auto_20191129_1427.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('orders', '0002_dinnerplatters_pasta_regularpizza_salad_sicilianpizza_sub_toppings'),
+        ('orders', '0002_manjus'),
     ]
 
     operations = [

03170121e1cc1afe69cf5c661085b697091f217c
unmexicano <39681418+unmexicano@users.noreply.github.com>
2022-02-15 20:00:50 -0600

    Update urls.py

--- pizza/urls.py
+++ pizza/urls.py
@@ -20,5 +20,4 @@ from django.urls import include, path
 urlpatterns = [
     path("", include("orders.urls")),
     path("admin/", admin.site.urls),
-    path('tinymce/', include('tinymce.urls')),
 ]

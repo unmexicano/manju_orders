9a1bedbad9e81f4fe13b9b5588c973e94559257e
angelserrano <angelserranogs@gmail.com>
2022-02-16 23:18:37 -0600

    update

--- orders/admin.py
+++ orders/admin.py
@@ -1,17 +1,19 @@
 from django.contrib import admin
 from .models import opciones, Category, UserOrder, SavedCarts
-from tinymce import models as tinymce_models
+from tinymce.widgets import TinyMCE
 from django.db import models
 
 
 class categoriaAdmin(admin.ModelAdmin):
-    formfield_overrides = {
-                              tinymce_models.HTMLField()},
-
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
 
 class opcionesAdmin(admin.ModelAdmin):
-    formfield_overrides = {
-                               tinymce_models.HTMLField()},
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
+
 
 
 
